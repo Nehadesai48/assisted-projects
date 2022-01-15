09054a4b8c5799eb73f09d22945ffc0df56f1bae
Nehadesai48 <97732938+Nehadesai48@users.noreply.github.com>
2022-01-15 13:29:25 +0530

    Add files via upload

---
+++ arithmeticalculator.java
@@ -0,0 +1,19 @@
+package practiceproject;
+
+public class arithmeticalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1=34;
+int num2=89;
+int ans=num1+num2;
+System.out.println("sum="+ans);
+ans=num1-num2;
+System.out.println("difference="+ans);
+ans=num1*num2;
+System.out.println("multiplication="+ans);
+ans=num1%num2;
+System.out.println("divide="+ans);
+	}
+
+}
